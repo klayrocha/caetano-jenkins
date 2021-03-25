3131abd130ccb60b84186a1065da70b01c36435e
Luis Barros <luis.p.barros.ext@spms.min-saude.pt>
2021-03-25 17:54:31 +0000

    mvn

---
+++ src/main/java/com/klayrocha/caetanojenkins/controller/JenkinsController.java
@@ -0,0 +1,15 @@
+package com.klayrocha.caetanojenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class JenkinsController {
+
+	@GetMapping
+	public String getHello() {
+		return "Caetano Jenkins";
+	}
+}
