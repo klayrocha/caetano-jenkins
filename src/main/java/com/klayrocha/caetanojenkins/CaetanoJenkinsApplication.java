fbceca1835e4aa7034ebf9bbd7076d0225f96a3e
Luis Barros <luis.p.barros.ext@spms.min-saude.pt>
2021-03-25 17:41:51 +0000

    first

---
+++ src/main/java/com/klayrocha/caetanojenkins/CaetanoJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.klayrocha.caetanojenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaetanoJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaetanoJenkinsApplication.class, args);
+	}
+
+}
